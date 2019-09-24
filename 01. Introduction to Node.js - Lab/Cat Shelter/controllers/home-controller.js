2c42bca96d8fb7f72d5e93e99ef08a6dbb2824af
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-09-24 11:27:23 +0300

    Added working app.

--- 01. Introduction to Node.js - Lab/Cat Shelter/controllers/home-controller.js
+++ 01. Introduction to Node.js - Lab/Cat Shelter/controllers/home-controller.js
@@ -7,27 +7,68 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname === '/' && req.method === 'GET') {
-        fs.readFile(rootPath + 'index.html', (err, data) => {
-            if (err) {
-              console.log(err);
-              res.writeHead(404, {
-                'Content-Type': 'text/plain'
+      const filePath = path.normalize(
+          path.join(__dirname, '../views/home/index.html')
+      );
+
+      fs.readFile('./data/cats.json', (err, data) => {
+          if (err) {
+              res.writeHead(500, {
+                  'Content-Type': 'text/plain'
               });
 
-              res.write('404 Not Found');
+              res.write(`An error has occured. (${err.code})`);
+              res.end();
+
+              throw err;
+          }
+          let cats = JSON.parse(data);
+
+          let modifiedCats = cats.map((cat) =>
+              `<li>
+              <img src="./public/images/${cat.image}" alt="${cat.name}">
+              <h3>${cat.name}</h3>
+              <p><span>Breed: </span>${cat.breed}</p>
+              <p><span>Description: </span>${cat.description}</p>
+              <ul class="buttons">
+                  <li class="btn edit"><a href="/edit-cat/${cat.id}">Change Info</a></li>
+                  <li class="btn delete"><a href="/delete-cat/${cat.id}">New Home</a></li>
+              </ul>
+          </li>`
+          );
+
+          fs.readFile(filePath, (err, data) => {
+              if (err) {
+                  if (err.code == 'ENOENT') {
+                      res.writeHead(404, {
+                          'Content-Type': 'text/plain'
+                      });
+
+                      res.write('The requested resource was not found.');
+                      res.end();
+                  } else {
+                      res.writeHead(500, {
+                          'Content-Type': 'text/plain'
+                      });
+
+                      res.write(`An error has occured. (${err.code})`);
+                      res.end();
+                  }
+                  return;
+              }
+
+              res.writeHead(200, {
+                  'Content-Type': `text/html`
+              });
+
+              let finalData = data.toString().replace('{{cats}}', modifiedCats);
+
+              res.write(finalData);
               res.end();
               return;
-            }
-    
-            res.writeHead(200, {
-              'content-type': 'text/html'
-            });
-
-            res.write(data);
-            res.end();
-        });
-    } else {
-        return true;
-    }
-
+          });
+      });
+  } else {
+      return true;
+  }
 }
--- 01. Introduction to Node.js - Lab/Cat Shelter/controllers/static-files-controller.js
+++ 01. Introduction to Node.js - Lab/Cat Shelter/controllers/static-files-controller.js
@@ -6,20 +6,24 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname.startsWith('/public') && req.method === 'GET') {
-        fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
+      if (pathname.endsWith('png') || pathname.endsWith('jpg') || pathname.endsWith('jpeg') || pathname.endsWith('ico')) {
+        fs.readFile(`./${pathname}`, (err, data) => {
             if (err) {
-                console.log(err);
-
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-
-                res.write('404 Not Found');
-                res.end();
-                return;
+                if (err.code === 'ENOENT') {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.write('The requested resource was not found.');
+                    res.end();
+                } else {
+                    res.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.write(`An error has occured. (${err.code})`);
+                    res.end();
+                }
             }
 
-            console.log(pathname);
             res.writeHead(200, {
                 'Content-Type': getContentType(pathname)
             });
@@ -27,6 +31,29 @@ module.exports = (req, res) => {
             res.write(data);
             res.end();
         });
+      } else {
+        fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
+          if (err) {
+              console.log(err);
+
+              res.writeHead(404, {
+                  'Content-Type': 'text/plain'
+              });
+
+              res.write('404 Not Found');
+              res.end();
+              return;
+          }
+
+          console.log(pathname);
+          res.writeHead(200, {
+              'Content-Type': getContentType(pathname)
+          });
+
+          res.write(data);
+          res.end();
+        });
+      }        
     } else {
         return true;
     }
